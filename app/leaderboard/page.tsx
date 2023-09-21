a538baf2cd3ea0f84e374b232c108f4ad1e89d61
witwiki <Vikram.Udyawer@community.isunet.edu>
2023-09-21 15:42:51 +1200

    completed basic routing between pages and metadata for SEO

--- app/leaderboard/page.tsx
+++ app/leaderboard/page.tsx
@@ -1,9 +1,22 @@
 import React from 'react'
+import type { Metadata } from 'next'
+// import { useRouter } from 'next/navigation'        {/** only works in client. Conflicts with metadata which only work in server mode: https://stackoverflow.com/questions/76353957/next-js-build-in-vercel-fails-when-with-meta-data-why */}
+
+export const metadata: Metadata = {
+  title: 'Dora Tournament Leaderboard',
+  description: 'Dora Data Science Tournaments Leaderboard',
+}
 
 const Leaderboard = () => {
+  // const router = useRouter();
   return (
     <div className='flex justify-between items-center h-full w-full px-4 2xl:px-16'>
       <h1 className="text-5xl font-extrabold dark:text-white">Leaderboard</h1>
+      {/* <div>
+        <button onClick={() => {
+          router.push("/")
+        }}></button>
+      </div> */}
     </div>
   )
 }
--- app/submissions/page.tsx
+++ app/submissions/page.tsx
@@ -1,4 +1,10 @@
 import React from 'react'
+import type { Metadata } from 'next'
+
+export const metadata: Metadata = {
+  title: 'Tournament Submissions',
+  description: 'Dora Data Science Tournaments Submissions',
+}
 
 const Submissions = () => {
   return (
