b550a3be640c3de2f35fcc9d73846eb946cda1b6
witwiki <Vikram.Udyawer@community.isunet.edu>
2023-10-02 16:01:57 +1300

    got started on the hero section

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,8 @@
 
 'use client'
 import React, { useEffect } from 'react'
+import Hero from './components/Hero'
+import CustomButton from './components/CustomButton'
 
 export default function Home() {
 
@@ -9,12 +11,30 @@ export default function Home() {
   // }, [])
 
   return (
-    // <main className="overflow-hidden">
-    <main>
-      <div className='flex justify-between items-center h-full w-full px-4 2xl:px-16'>
-        <h1 className="text-5xl font-extrabold dark:text-white">Home</h1>
+    <div className="hero">
+      <div className="flex-1 pt-36 padding-x">
+        <h1 className="hero__title">
+          The most vital data science tournament for humanity.
+        </h1>
+
+        <p className="hero__subtitle">
+          Predict Spacecraft Collision Risk
+        </p>
+
+        {/* <CustomButton
+          title="Explore Cars"
+          containerStyles="bg-primary-blue text-white rounded-full mt-10"
+          handleClick={handleScroll}
+        /> */}
       </div>
-    </main>
+      <div className="hero__image-container">
+        <div className="hero__image">
+          {/* <Image src="/hero.png" alt="hero" fill className="object-contain" /> */}
+        </div>
+
+        <div className="hero__image-overlay" />
+      </div>
+    </div>
     )
   }
   
